9ad9834812e263e772bf3f8e4d5950ee86a1fcbd
komolemo <131878827+komolemo@users.noreply.github.com>
2026-08-12 23:14:07 +0900

    <Button/>に「新規追加」用variantと主要操作用sizeを追加

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",
+        accent: "bg-create-new-button hover:bg-create-new-button-hover text-foreground",
       },
       size: {
         default:
@@ -30,6 +31,7 @@ const buttonVariants = cva(
         "icon-xs": "size-6 rounded-none [&_svg:not([class*='size-'])]:size-3",
         "icon-sm": "size-7 rounded-none",
         "icon-lg": "size-9",
+        action: "h-8 pl-2 pr-4 py-1 rounded-md"
       },
     },
     defaultVariants: {
